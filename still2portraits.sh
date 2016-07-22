80de9faf8ff9d8a238e34483e4db41cf44f91ad1
Janzen Brewer <janzen@toypiper.com>
2016-07-22 14:15:58 -0700

    Add argument and tweak file name scheme

--- still2portraits.sh
+++ still2portraits.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # this is meant to be used with xargs for batch mode
-# usage example: still2portraits.sh still0013.png still0013_
-# with xargs: ls | xargs -I file ../still2portraits.sh file file_
+# usage example: still2portraits.sh still0013.png still0013
+# with xargs: ls | xargs -I file ../still2portraits.sh file file
 
 # 480 values
 #CROP_X_STARTS=(15 53 90 127 164 202 404 442 479 517 554 591)
@@ -20,6 +20,6 @@ CONVERT=$(which convert)
 
 for NUM in $(seq 0 11); do
     CROP_X_START=${CROP_X_STARTS[$NUM]}
-    OUT_FILENAME=${2}_${NUM}.png
+    OUT_FILENAME=${NUM}_${2}.png
     $CONVERT $1 -crop ${CROP_X_SIZE}x${CROP_Y_SIZE}+${CROP_X_START}+${CROP_Y_START} +repage $OUT_FILENAME
 done
--- video2stills.sh
+++ video2stills.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# usage example: video2stills foo.mp4 1/5
+# params: video2stills <video file> <fps> <output filename prefix>
+# usage example: video2stills foo.mp4 1/5 out
 
-ffmpeg -i "$1" -vf fps=$2 out%04d.png
+ffmpeg -i "$1" -vf fps=$2 $3%04d.png
