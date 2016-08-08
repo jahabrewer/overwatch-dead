99995e9469a59ee4f1be0bc24fba6c47dc32b041
Janzen Brewer <janzen@toypiper.com>
2016-08-07 20:59:34 -0400

    Add instrumentation and shuffling to trainer

--- train.py
+++ train.py
@@ -27,6 +27,7 @@ from __future__ import division
 from __future__ import print_function
 import argparse
 import sys
+import collections
 
 # Import data
 # from tensorflow.examples.tutorials.mnist import input_data
@@ -58,13 +59,14 @@ parser.add_argument("--dropout", "-d", type=float, help="probability that a neur
 parser.add_argument("--summaries-dir", "-s", help="the directory in which to write summary logs", \
   type=str, default="/tmp/overwatch-dead-logs")
 parser.add_argument("-v", "--verbose", action="store_true")
+parser.add_argument("-t", "--run-test", action="store_true", help="run on test data at the end")
 args = parser.parse_args()
 
 # TODO get rid of these constants
 IMAGE_HEIGHT=24
 IMAGE_WIDTH=24
 IMAGE_CHANNELS=3
-NUM_CLASSES=2
+NUM_CLASSES=24
 
 # flags = tf.app.flags
 # FLAGS = flags.FLAGS
@@ -225,16 +227,23 @@ def read_data_sets(train_image_file,
     test = fake()
     return base.Datasets(train=train, validation=validation, test=test)
 
-  # TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
-  # TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
-  # TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
-  # TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
-  # VALIDATION_SIZE = 5000
-
   train_images = extract_images(train_image_file)
   train_labels = extract_labels(train_label_file, one_hot=one_hot)
   test_images = extract_images(test_image_file)
   test_labels = extract_labels(test_label_file, one_hot=one_hot)
+
+  # TODO use shuffle_batch?
+  # Shuffle the data.
+  train_size = train_images.shape[0]
+  train_shuffle_indices = numpy.array(range(train_size))
+  numpy.random.shuffle(train_shuffle_indices)
+  train_images = train_images[train_shuffle_indices, ...]
+  train_labels = train_labels[train_shuffle_indices, ...]
+  test_size = test_images.shape[0]
+  test_shuffle_indices = numpy.array(range(test_size))
+  numpy.random.shuffle(test_shuffle_indices)
+  test_images = test_images[test_shuffle_indices, ...]
+  test_labels = test_labels[test_shuffle_indices, ...]
 
   validation_images = train_images[:validation_size]
   validation_labels = train_labels[:validation_size]
@@ -246,7 +255,23 @@ def read_data_sets(train_image_file,
                        validation_labels,
                        dtype=dtype,
                        reshape=reshape)
-  test = DataSet(test_images, test_labels, dtype=dtype, reshape=reshape)
+  if args.run_test:
+    test = DataSet(test_images, test_labels, dtype=dtype, reshape=reshape)
+  else:
+    test = None
+
+  # Print label composition of sets
+  if args.verbose:
+    print('Label composition')
+    for set_name, labels in [('train', train_labels), ('validation', validation_labels), ('test', test_labels)]:
+      print(set_name)
+      counts = collections.Counter([l.argmax() for l in labels])
+
+      for label_index in range(NUM_CLASSES):
+        print('{0}: {1}'.format(label_index, counts[label_index]))
+
+      print()
+
 
   return base.Datasets(train=train, validation=validation, test=test)
 
@@ -287,82 +312,108 @@ mnist = read_data_sets(args.train_image_file,
                        one_hot=True)
 
 with tf.Session() as sess:
-  # sess = tf.InteractiveSession()
 
-  with tf.name_scope('input')
-  x = tf.placeholder(tf.float32, shape=[None, IMAGE_HEIGHT*IMAGE_WIDTH*IMAGE_CHANNELS])
-  x_image = tf.reshape(x, [-1,IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_CHANNELS])
-  tf.image_summary('xinput', x_image, max_images=3)
-  y_ = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
+  with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, shape=[None, IMAGE_HEIGHT*IMAGE_WIDTH*IMAGE_CHANNELS])
+    x_image = tf.reshape(x, [-1,IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_CHANNELS])
+    # tf.image_summary('xinput', x_image, max_images=3)
+    y_ = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
 
-  #W = tf.Variable(tf.zeros([784,10]))
-  #b = tf.Variable(tf.zeros([10]))
   with tf.name_scope('conv1'):
     W_conv1 = weight_variable([5, 5, IMAGE_CHANNELS, 32])
-    variable_summaries(W_conv1, 'convv1/weights')
+    variable_summaries(W_conv1, 'conv1/weights')
     b_conv1 = bias_variable([32])
+    variable_summaries(b_conv1, 'conv1/biases')
 
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-  W_conv2 = weight_variable([5, 5, 32, 64])
-  # variable_summaries(W_conv2, 'conv2/weights')
-  b_conv2 = bias_variable([64])
+  with tf.name_scope('conv2'):
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    variable_summaries(W_conv2, 'conv2/weights')
+    b_conv2 = bias_variable([64])
+    variable_summaries(b_conv2, 'conv2/biases')
 
-  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-  h_pool2 = max_pool_2x2(h_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
-  # W_fc1 = weight_variable([IMAGE_HEIGHT/4 * IMAGE_WIDTH/4 * 64, 1024])
-  pooled_dimension = int(IMAGE_HEIGHT/4 * IMAGE_WIDTH/4 * 64)
-  W_fc1 = weight_variable([pooled_dimension, 1024])
-  b_fc1 = bias_variable([1024])
+  with tf.name_scope('fc1'):
+    pooled_dimension = IMAGE_HEIGHT//4 * IMAGE_WIDTH//4 * 64
+    W_fc1 = weight_variable([pooled_dimension, 1024])
+    variable_summaries(W_fc1, 'fc1/weights')
+    b_fc1 = bias_variable([1024])
+    variable_summaries(b_fc1, 'fc1/biases')
 
-  h_pool2_flat = tf.reshape(h_pool2, [-1, pooled_dimension])
-  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, pooled_dimension])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-  keep_prob = tf.placeholder(tf.float32)
-  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+  with tf.name_scope('dropout'):
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-  W_fc2 = weight_variable([1024, NUM_CLASSES])
-  b_fc2 = bias_variable([NUM_CLASSES])
+  with tf.name_scope('fc2'):
+    W_fc2 = weight_variable([1024, NUM_CLASSES])
+    variable_summaries(W_fc2, 'fc2/weights')
+    b_fc2 = bias_variable([NUM_CLASSES])
+    variable_summaries(b_fc2, 'fc2/biases')
 
-  y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+  y=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
   with tf.name_scope('cross_entropy'):
-    diff = y_ * tf.log(y_conv)
-    # diff = y_ * tf.log(tf.clip_by_value(y_conv,1e-10,1.0))
+    diff = y_ * tf.log(y)
     with tf.name_scope('total'):
       cross_entropy = -tf.reduce_mean(diff)
-      cross_entropy = tf.Print(cross_entropy, [cross_entropy], "CrossE")
     tf.scalar_summary('cross entropy', cross_entropy)
-  # cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
   train_step = tf.train.AdamOptimizer(args.learning_rate).minimize(cross_entropy)
-  correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+  correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  tf.scalar_summary('accuracy', accuracy)
 
   # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
   merged = tf.merge_all_summaries()
   train_writer = tf.train.SummaryWriter(args.summaries_dir + '/train',
                                         sess.graph)
+  validation_writer = tf.train.SummaryWriter(args.summaries_dir + '/validation')
   test_writer = tf.train.SummaryWriter(args.summaries_dir + '/test')
 
-  def feed_dict(train):
+  # log the incorrectly predicted images
+  incorrect_prediction = tf.not_equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  incorrect_prediction_indices = tf.where(incorrect_prediction)
+  # the flattened images that were incorrectly predicted
+  incorrect_x_flat = tf.gather(x, incorrect_prediction_indices)
+  # the correctly shaped images
+  incorrect_x = tf.reshape(incorrect_x_flat, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
+  # the (incorrectly) predicted classes for these images
+  incorrect_y = tf.gather(tf.argmax(y, 1), incorrect_prediction_indices)
+  incorrect_y = tf.Print(incorrect_y, [incorrect_y], "inc y")
+  incorrect_summary_op = tf.image_summary("incorrect", incorrect_x, max_images=100)
+
+  def feed_dict(dataset):
     """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
-    if train:
+    if dataset == 'train':
       xs, ys = mnist.train.next_batch(args.batch_size)
       k = args.dropout
-    else:
+    elif dataset == 'validation':
+      xs, ys = mnist.validation.images, mnist.validation.labels
+      k = 1.0
+    elif dataset == 'test':
       xs, ys = mnist.test.images, mnist.test.labels
       k = 1.0
+    else:
+      raise ValueError('unknown dataset: {0}'.format(dataset))
     return {x: xs, y_: ys, keep_prob: k}
 
   sess.run(tf.initialize_all_variables())
 
   for i in range(args.num_steps):
     if i % args.checkpoint_interval == 0:  # Record summaries and test-set accuracy
-      summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(train=False))
-      test_writer.add_summary(summary, i)
-      print('Accuracy at step %s: %s' % (i, acc))
+      summary, acc, incorrect_summary, _ = sess.run([merged, accuracy, incorrect_summary_op, incorrect_y],
+        feed_dict=feed_dict('validation'))
+      validation_writer.add_summary(summary, i)
+      # only log incorrect images on the last checkpoint
+      if i >= (args.num_steps - args.checkpoint_interval):
+        validation_writer.add_summary(incorrect_summary, i)
+      print('Validation accuracy at step %s: %s' % (i, acc))
     else:  # Record train set summaries, and train
       print('.', end='')
       sys.stdout.flush()
@@ -370,14 +421,17 @@ with tf.Session() as sess:
         run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
         run_metadata = tf.RunMetadata()
         summary, _ = sess.run([merged, train_step],
-                              feed_dict=feed_dict(train=True),
+                              feed_dict=feed_dict('train'),
                               options=run_options,
                               run_metadata=run_metadata)
         train_writer.add_run_metadata(run_metadata, 'step%d' % i)
         train_writer.add_summary(summary, i)
-        print('Adding run metadata for', i)
+        # print('Adding run metadata for', i)
       else:  # Record a summary
-        summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(train=True))
+        summary, _ = sess.run([merged, train_step], feed_dict=feed_dict('train'))
         train_writer.add_summary(summary, i)
 
-  print("test accuracy %g"%accuracy.eval(feed_dict=feed_dict(train=False)))
+  if args.run_test:
+    acc, incorrect_summary, _ = sess.run([accuracy, incorrect_summary_op, incorrect_y], feed_dict=feed_dict('test'))
+    test_writer.add_summary(incorrect_summary, args.num_steps)
+    print("Final test accuracy {0}".format(acc))
